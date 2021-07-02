4b6920c51848dfdc43304cbb7feb62e96701f2ea
JuiceWAH10 <jjamora121014@gmail.com>
2021-07-03 02:31:04 +0800

    new files

---
+++ App/models/products.js
@@ -0,0 +1,9 @@
+class Product{
+    constructor(product_ID, shop_ID, product_Name, price, stock){
+        this.product_ID = product_ID;
+        this.shop_ID = shop_ID;
+        this.product_Name = product_Name;
+        this.price = price;
+        this.stock = stock;
+    }
+}
